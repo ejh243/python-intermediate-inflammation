3a388b3364f134e1dbfea0f6f76879eda399700e
Hannon <E.J.Hannon@exeter.ac.uk>
2021-12-06 12:18:49 +0000

    fix style errors

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -17,11 +17,13 @@ def main(args):
     if not isinstance(in_files, list):
         in_files = [args.infiles]
 
-
     for filename in in_files:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {'average': models.daily_mean(inflammation_data),
+                     'max': models.daily_max(inflammation_data),
+                     'min': models.daily_min(inflammation_data)
+                     }
 
         views.visualize(view_data)
 
@@ -37,3 +39,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args)
+
