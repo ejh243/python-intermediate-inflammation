695dd08bad8bffac11059c66f7c72347802c0f60
Hannon <E.J.Hannon@exeter.ac.uk>
2021-12-07 11:23:43 +0000

    add tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
+
 
 
 def test_daily_mean_zeros():
@@ -76,5 +78,13 @@ def test_daily_min_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_min(test_input), test_result)
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
 
 # TODO(lesson-robust) Implement tests for the other statistical functions
